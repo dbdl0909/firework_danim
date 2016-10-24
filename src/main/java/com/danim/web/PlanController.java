1011c572c13ec2d76a2440910767e9ae70fdb9a9
dbdl0909 <rydn153123@naver.com>
2016-10-24 14:01:31 +0900

    plan 쪽 수정

--- src/main/java/com/danim/web/PlanController.java
+++ src/main/java/com/danim/web/PlanController.java
@@ -1,5 +1,7 @@
 package com.danim.web;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,36 +10,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.danim.service.plan.CityInfoDto;
 import com.danim.service.plan.CityInfoService;
 
 @Controller
 public class PlanController {
-	
 	private static final Logger logger = LoggerFactory.getLogger(PlanController.class);
 	
 	@Autowired
     private CityInfoService cityInfoService;
+	
 	Model model;
+	
 	@RequestMapping(value = "/plan/mainPlan")
 	public String mainPlan(Model model, @RequestParam(value="do")String doArea) {
-		logger.info("도시이름: {}", doArea);
+		//지도 그림에서 도를 클릭했을때 do 담겨진 값을 doArea에 담는다.
+		logger.info("doArea(도 이름) : {} <-- mainPlan() PlanController.java", doArea);
 		
-		logger.info("모델:{}",cityInfoService.selectcityInfo(doArea));
-		model.addAttribute("listCityInfoDto", cityInfoService.selectcityInfo(doArea));
+		//doArea(도 이름)의 값을 매개변수에 담아 해당 도의 도시들을 리스트에 담아 가져온다.
+		List<CityInfoDto> listCityInfoByCityInfoDoName = cityInfoService.selectCityInfoByCityInfoDoName(doArea);
+		//가져온 리스트를 jsp 페이지에서 출력하기 위해 model에 담는다. 
+		model.addAttribute("listCityInfoByCityInfoDoName", listCityInfoByCityInfoDoName);
 		
 		return "plan/mainPlan";
 	}
-	@RequestMapping(value = "/plan/planTest")
+	
+	/*@RequestMapping(value = "/plan/planTest")
 	public String planTest() {
 		logger.info("Welcome planTest! The client locale is {}.");
 		
 		
 		return "plan/planTest";
-	}
-	
-	@RequestMapping(value = "/plan/city")
-	public String cityInfo() {
-		return null;
-	}
+	}*/
 
 }
