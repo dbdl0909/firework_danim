232045b7963d8ce4187bb4b7490cb27e81411429
yangkyunghwa <yyyyy519@naver.com>
2016-10-26 09:07:08 +0900

    memberDao 수정

--- src/main/java/com/danim/service/member/MemberDao.java
+++ src/main/java/com/danim/service/member/MemberDao.java
@@ -4,14 +4,15 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MemberDao {
 	private static final Logger logger = LoggerFactory.getLogger(MemberDao.class);
 	
 	@Autowired
 	private SqlSessionTemplate sessionTemplate;
-
-	@Autowired
+	
 	private final String namesPace = "com.danim.service.MemberMapper";
 	
 	public void insertMember(MemberDto memberDto) {
