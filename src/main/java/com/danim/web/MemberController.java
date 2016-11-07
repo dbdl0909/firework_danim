949b29bf3065c2f337a4e8d9412d4b2190d05934
yangkyunghwa <yyyyy519@naver.com>
2016-11-07 10:02:56 +0900

    나중에 헤더 추가하기.... 수빈짱때문에 못함 ㅠㅠ

--- src/main/java/com/danim/web/MemberController.java
+++ src/main/java/com/danim/web/MemberController.java
@@ -1,7 +1,7 @@
 package com.danim.web;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -25,7 +25,14 @@ public class MemberController {
 	
 	@Autowired
 	private MemberService memberService;
-	
+	//로그아웃
+	@RequestMapping(value="/member/memberLogout", method = RequestMethod.GET)
+	public String MemberLogout(HttpSession session) {
+		String logoutTime = new SimpleDateFormat("yyyy/mm/dd/hh:mm").format(new Date());
+		logger.info("logoutTime : {}", logoutTime);
+		session.invalidate();	//세션 종료(데이터 전부 삭제)
+		return "redirect:/";
+	}
 	//로그인
 	@RequestMapping(value="/member/memberLoginSubmit", method = RequestMethod.POST)
 	public String MemberLogin(HttpServletRequest request,
@@ -40,10 +47,9 @@ public class MemberController {
 			HttpSession session  = request.getSession(true);
 			session.setAttribute("memberId", memberId);
 		}
-		
 		return "redirect:/";
 	}
-	//로그인폼 실행
+	//로그인폼
 	@RequestMapping(value="/member/memberLoginForm", method = RequestMethod.GET)
 	public String memberLogin(Model model) {
 		logger.info("memberLogin MemberController.java");
@@ -53,9 +59,7 @@ public class MemberController {
 	@RequestMapping(value="/member/memberListAll", method = RequestMethod.POST)
 	public String memberListAll(Model model) {
 		logger.info("memberListAll MemberController.java");
-		
 		model.addAttribute("selectMemberAll", memberService.selectMemberAll());
-		
 		return "member/memberListAll";
 	}
 	//입력 폼
@@ -68,9 +72,7 @@ public class MemberController {
 	public String memberInsert(Model model, MemberDto memberDto) {
 		logger.info("memberInsert MemberController.java");
 		logger.info("MemberDto : {}", memberDto);
-		
 		memberService.insertMember(memberDto);
-		
 		return "redirect:/";
 	}
 }
